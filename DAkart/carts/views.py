99bc991012a06e6ecf07f5fb83829be14051ac36
= <=>
2023-11-01 09:44:09 +0530

    carts code

--- DAkart/carts/views.py
+++ DAkart/carts/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import json
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
 from store.models import Product
 from carts.models import Cart, CartItem
 
@@ -11,37 +12,42 @@ def _cart_id(request):
     if not cart_session_key:
         cart_session_key = request.session.create()
     return cart_session_key
+    
 
 def add_cart(request,product_id):
     product = Product.objects.get(id=product_id)
-    
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart = Cart.objects.get(cart_id =_cart_id(request))
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-
-        cart.save()
-    
+         cart = Cart.objects.create(
+             cart_id = _cart_id(request)
+         )
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity = 1
+        cart_item = CartItem.objects.get(product = product,cart = cart)
+        cart_item.quantity +=1
         cart_item.save()
-
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
             product = product,
             quantity = 1,
             cart = cart
         )
+        
 
-        cart_item.save()
+    return redirect('cart')
     
     
+def cart(request, total =0 , quantity = 1, cart_items=None):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    cart_items = CartItem.objects.filter(cart=cart)
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity )
+    
+    context = {
+        'cartItems':cart_items,
+        'total':total
+    }
+    return render(request,'cart.html',context)
 
 
-
-
-def cart(request):
-    return render(request,'cart.html')
